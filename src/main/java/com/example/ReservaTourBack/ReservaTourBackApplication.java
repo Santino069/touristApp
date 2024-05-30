125186af311e422714b2bca9e8fc3f73c0d63b6a
= <=>
2024-05-30 01:23:47 -0500

    Nuevos Cambios

--- src/main/java/com/example/ReservaTourBack/ReservaTourBackApplication.java
+++ src/main/java/com/example/ReservaTourBack/ReservaTourBackApplication.java
@@ -22,7 +22,7 @@ public class ReservaTourBackApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://192.168.1.6:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://192.168.1.6:4200", "https://reservatourfrontend.web.app"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
